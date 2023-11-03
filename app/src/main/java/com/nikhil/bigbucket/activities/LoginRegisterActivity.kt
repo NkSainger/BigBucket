1fb877436372be1e8b793da4bc83e873ae1357af
nikhil <nikhilsainger787@gmail.com>
2023-11-03 19:32:30 +0530

    Initial version with splash screen, login, register fragments.

---
+++ app/src/main/java/com/nikhil/bigbucket/activities/LoginRegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.nikhil.bigbucket.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nikhil.bigbucket.R
+
+class LoginRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_register)
+    }
+}
---
+++ app/src/main/java/com/nikhil/bigbucket/activities/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.nikhil.bigbucket.activities
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.nikhil.bigbucket.R
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@SplashActivity, LoginRegisterActivity::class.java)
+            startActivity(intent)
+        }, 2000)
+    }
+}
